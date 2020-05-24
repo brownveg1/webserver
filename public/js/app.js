1b7e1f9a590ca22993c4c837dd160d5ec82c7fd9
deepak <deepak@brownveg>
2020-05-23 22:29:40 -0700

    setup for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -13,27 +13,7 @@ weatherForm.addEventListener('submit', (e) => {
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
 
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
-        response.json().then((data) => {
-            if (data.error) {
-                messageOne.textContent = data.error
-            } else {
-                messageOne.textContent = data.location
-                messageTwo.textContent = data.forecast
-            }
-        })
-    })
-})
-
-orderForm.addEventListener('submit', (e) => {
-    e.preventDefault()
-
-    const orderno = search.value
-
-    messageOne.textContent = 'Loading...'
-    messageTwo.textContent = ''
-
-    fetch('http://localhost:3000/order?orderno=' + orderid).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 messageOne.textContent = data.error
